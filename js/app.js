470f727d995aea1c72b4db334bcee12eb150cdcd
rickey.liao <micliao@gmail.com>
2022-05-20 20:59:24 +0800

    add to repo

--- js/app.js
+++ js/app.js
@@ -12,21 +12,7 @@ window.onload = async () => {
     await start()
 }
 
-Date.prototype.Format = function (fmt) { 
-    var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "h+": this.getHours(), //小时 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
-}
+
 
 
 
@@ -129,6 +115,23 @@ async function injectContractBaseInfo() {
     $("#udid_balance").html(""+String(bf))
     console.log("lasttime",window.app.lastAccTime)
     if (parseInt(window.app.lastAccTime) > 0){
+
+        Date.prototype.Format = function (fmt) { 
+            var o = {
+                "M+": this.getMonth() + 1, //月份 
+                "d+": this.getDate(), //日 
+                "h+": this.getHours(), //小时 
+                "m+": this.getMinutes(), //分 
+                "s+": this.getSeconds(), //秒 
+                "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+                "S": this.getMilliseconds() //毫秒 
+            };
+            if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+            for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            return fmt;
+        }
+
         var ld = Date(parseInt(window.app.lastAccTime)*1000)
         $("#last_Time").html(ld.Format("yyyy-MM-dd hh:mm"));
         console.log(ld.toLocaleString())
@@ -177,6 +180,21 @@ async function syncBalance() {
         console.log("lasttime",window.app.lastAccTime)
 
         if (parseInt(window.app.lastAccTime) > 0){
+            Date.prototype.Format = function (fmt) { 
+                var o = {
+                    "M+": this.getMonth() + 1, //月份 
+                    "d+": this.getDate(), //日 
+                    "h+": this.getHours(), //小时 
+                    "m+": this.getMinutes(), //分 
+                    "s+": this.getSeconds(), //秒 
+                    "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+                    "S": this.getMilliseconds() //毫秒 
+                };
+                if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+                for (var k in o)
+                if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+                return fmt;
+            }
             var ld = Date(parseInt(window.app.lastAccTime)*1000)
             $("#last_Time").html(ld.toLocaleString());
             console.log(""+ld.Format("yyyy-MM-dd hh:mm"))
